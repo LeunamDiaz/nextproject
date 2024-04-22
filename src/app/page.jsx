5e4b90d1841ddbbf101b7d4e458a4d75db5e7612
marshallog <leunamdiazmx@gmail.com>
2024-04-22 02:37:12 -0600

    Primer commit: inicializando proyecto Next.js y corrigiendo errores de layout.jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function IndexPage() {
+  return (
+    <div>
+        <h1>Index</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus molestiae consequuntur illum, eaque asperiores ut odit vero quam obcaecati fuga sequi impedit animi voluptas quidem. Eaque, voluptas ullam pariatur iusto sapiente excepturi, debitis ut temporibus dicta porro tempora veniam ratione laboriosam, ducimus enim atque quo eligendi doloremque aliquid quia! Nobis!</p>
+    </div>
+  )
+}
+
+export default IndexPage
---
+++ src/app/services/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ServicesPage() {
+  return (
+    <div>
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus molestiae consequuntur illum, eaque aspe</p>
+    </div>
+  )
+}
+
+export default ServicesPage
